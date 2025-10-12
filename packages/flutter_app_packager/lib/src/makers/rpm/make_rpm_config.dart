1198c7e90782e111759666aff4eaf1e8578d68b1
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2025-10-12 08:57:19 +0800

    Fixed RPM SPEC file Changelog not being read issue

--- packages/flutter_app_packager/lib/src/makers/rpm/make_rpm_config.dart
+++ packages/flutter_app_packager/lib/src/makers/rpm/make_rpm_config.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter_app_packager/src/api/app_package_maker.dart';
+import 'package:intl/intl.dart';
 
 class MakeRPMConfig extends MakeConfig {
   MakeRPMConfig({
@@ -92,6 +93,7 @@ class MakeRPMConfig extends MakeConfig {
   String? buildArch;
   List<String>? requires;
   List<String>? buildRequires;
+
   //RPM postamble Spec file fields
   String? description;
   String? prep;
@@ -105,6 +107,9 @@ class MakeRPMConfig extends MakeConfig {
 
   @override
   Map<String, dynamic> toJson() {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('EEE MMM dd yyyy').format(now);
+    final rpmVersion = appVersion.toString();
     return {
       'SPEC': {
         'preamble': {
@@ -131,12 +136,13 @@ class MakeRPMConfig extends MakeConfig {
             'mkdir -p %{buildroot}%{_datadir}/applications',
             'mkdir -p %{buildroot}%{_datadir}/metainfo',
             'mkdir -p %{buildroot}%{_datadir}/pixmaps',
-            'cp -r %{name}/* %{buildroot}%{_datadir}/%{name}',
+            'cp -r ../%{name}/* %{buildroot}%{_datadir}/%{name}',
             'ln -s %{_datadir}/%{name}/%{name} %{buildroot}%{_bindir}/%{name}',
-            'cp -r %{name}.desktop %{buildroot}%{_datadir}/applications',
-            'cp -r %{name}.png %{buildroot}%{_datadir}/pixmaps',
-            'cp -r %{name}*.xml %{buildroot}%{_datadir}/metainfo || :',
+            'cp -r ../%{name}.desktop %{buildroot}%{_datadir}/applications',
+            'cp -r ../%{name}.png %{buildroot}%{_datadir}/pixmaps',
+            'cp -r ../%{name}*.xml %{buildroot}%{_datadir}/metainfo || :',
             'update-mime-database %{_datadir}/mime &> /dev/null || :',
+            '%undefine __brp_add_determinism',
           ].join('\n'),
           '%postun': ['update-mime-database %{_datadir}/mime &> /dev/null || :']
               .join('\n'),
@@ -150,6 +156,8 @@ class MakeRPMConfig extends MakeConfig {
         'inline-body': {
           '%defattr': '(-,root,root)',
           '%attr': '(4755, root, root) %{_datadir}/pixmaps/%{name}.png',
+          '%changelog':
+              '\n* $formattedDate $packager <$packagerEmail> - $rpmVersion\n- $changelog',
         },
       },
       'DESKTOP': {
